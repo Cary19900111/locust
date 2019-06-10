fac972685485049fc7d568208c8976c500da4407
Cary19900111 <1516945385@qq.com>
2019-06-10 20:05:50 +0800

    mergepoint

--- mysite/test/testperform.py
+++ mysite/test/testperform.py
@@ -1,8 +1,8 @@
 from locust import HttpLocust,TaskSet,task
-#locust -f testcc/g7.py --no-web --run-time 10s --csv result -c 5 -r 1
+#python crlocust.py -f mysite/test/testperform.py --no-web --run-time 5s --csv result -c 5 -r 1
 from locust import events
+from gevent._semaphore import Semaphore
 import datetime 
-
 
 '''写的钩子函数'''
 # from locust import Locust, TaskSet, events, task
@@ -31,33 +31,32 @@ import datetime
 #     max_wait = 3000
 #     stop_timeout = 10000
 '''设置集合点Semaphore'''
-# from gevent._semaphore import Semaphore
-# all_locusts_spawned = Semaphore()
-# all_locusts_spawned.acquire()
-# all_locusts_spawned.wait()#放在要集合的地方
+all_locusts_spawned = Semaphore()#信号量，等待全部启动完毕（hatch_complete）后，释放release信号量
+all_locusts_spawned.acquire()
 
-# def on_hatch_complete(**kwargs):
-#     all_locusts_spawned.release()
+def on_hatch_complete(**kwargs):
+    all_locusts_spawned.release()
+events.hatch_complete += on_hatch_complete
 
 
-# def request_success_hook(request_type, name, response_time, response_length):
-#     with open ("result.txt",'a+') as f:
-#         now_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-#         msg = "{}  {}  {}  {}  {}  {}\n".format(now_time,"SUCCESS",request_type,name,response_time,response_length)
-#         f.write(msg)
-# def request_failure_hook(request_type, name, response_time, exception):
-#     with open ("result.txt",'a+') as f:
-#         now_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-#         msg = "{}  {}  {}  {}  {}  {}\n".format(now_time,"FAIL",request_type,name,response_time,exception)
-#         f.write(msg)
-# events.request_success+=request_success_hook
-# events.request_failure+=request_failure_hook
+def request_success_hook(request_type, name, response_time, response_length):
+    with open ("CRRstSucess.txt",'a+') as f:
+        now_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        msg = "{}  {}  {}  {}  {}  {}\n".format(now_time,"SUCCESS",request_type,name,response_time,response_length)
+        f.write(msg)
+def request_failure_hook(request_type, name, response_time, exception):
+    with open ("CRRstFail.txt",'a+') as f:
+        now_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        msg = "{}  {}  {}  {}  {}  {}\n".format(now_time,"FAIL",request_type,name,response_time,exception)
+        f.write(msg)
+events.request_success+=request_success_hook
+events.request_failure+=request_failure_hook
 class TestPerform(TaskSet):
+    def on_start(self):
+        all_locusts_spawned.wait()
     @task
     def smoke(self):
-        response = self.client.get(url="/perform",name="测试",catch_response=True)
-        # if response.status_code=="200":
-        response.failure('!=201')
+        response = self.client.get(url="/perform",name="测试",catch_response=False)
 class WebUser(HttpLocust):
     # task_set = [ShoutTask,DonwTask]
     host = "http://127.0.0.1:8000"
